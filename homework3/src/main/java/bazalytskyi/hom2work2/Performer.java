8f183d59d6debebad24808a05cbc611b2767a672
BazaUA <vbazalickyi@gmail.com>
2018-02-10 00:17:34 +0200

    hw3

---
+++ homework3/src/main/java/bazalytskyi/hom2work2/Performer.java
@@ -0,0 +1,5 @@
+package bazalytskyi.hom2work2;
+
+public interface Performer {
+	void perform();
+}
---
+++ homework3/src/main/java/bazalytskyi/hom2work2/Stage.java
@@ -0,0 +1,14 @@
+package bazalytskyi.hom2work2;
+
+public class Stage {
+	private Stage() {
+	}
+
+	private static class StageSingletonHolder {
+		static Stage instance = new Stage();
+	}
+
+	public static Stage getInstance() {
+		return StageSingletonHolder.instance;
+	}
+}
